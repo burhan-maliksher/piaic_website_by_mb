ef1fdda187e793b20ffcfa205a678b89446a5e79
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-03-18 11:05:39 +0500

    trying to move programs from hero section

--- app/components/programheading.tsx
+++ app/components/programheading.tsx
@@ -6,7 +6,7 @@ import { Arrow, Qoute1, Qoute2 } from './icons'
 export default function Programheading() {
   return (
       <div className='self-center'>
-        <div className='flex gap-2 mt-12 '>
+        <div className='flex gap-2 mt-8 md:mt-12 lg:mt-16 xl:mt-24 '>
           <Qoute1/>
           <motion.h2 className='font-iceberg text-emerald-300 text-base lg:text-xl self-center drop-shadow-[10px_20px_4px_rgba(255,255,255,1)] shadow-white'
           initial={{opacity:0}}
